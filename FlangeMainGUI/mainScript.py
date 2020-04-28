fe3a14c17e855ee5962e1a2abd25a3bd11dd3299
Josh Collins <jcollins997@yahoo.com>
2020-04-28 17:18:48 -0500

    Fixed import problem in mainScript

--- FlangeMainGUI/mainScript.py
+++ FlangeMainGUI/mainScript.py
@@ -1,21 +1,8 @@
-import os
-import imp
-
-# cwd = os.getcwd()
-# print(cwd)
-
-HOME = os.environ.get("HOMEDRIVE")
-USER = os.environ.get("USERNAME")
-
-Flange = imp.load_source('FlangeWithThickComplexHub', HOME+"\\Users\\"+USER+"\\abaqus_plugins\\FlangeMainGUI\\FlangeWithThickComplexHub.py")
-Bolt = imp.load_source('createBolt', HOME+"\\Users\\"+USER+"\\abaqus_plugins\\FlangeMainGUI\\createBolt.py")
-Gasket = imp.load_source('createGasket', HOME+"\\Users\\"+USER+"\\abaqus_plugins\\FlangeMainGUI\\createGasket.py")
-Mapper = imp.load_source('csv_reader', HOME+"\\Users\\"+USER+"\\abaqus_plugins\\FlangeMainGUI\\csv_reader.py")
-Assembly = imp.load_source('assembly', HOME+"\\Users\\"+USER+"\\abaqus_plugins\\FlangeMainGUI\\assembly.py")
-
-
-# Flange = imp.load_source('FlangeWithThickComplexHub', "C:\\Users\\yakov\\abaqus_plugins\\FlangeMainGUI\\FlangeWithThickComplexHub.py")
-# Mapper = imp.load_source('FlangeWithThickComplexHub', "C:\\Users\\yakov\\abaqus_plugins\\FlangeMainGUI\\csv_reader.py")
+import makeAssembly as Assembly
+import createBolt as Bolt
+import createGasket as Gasket
+import FlangeWithThickComplexHub as Flange
+import csv_reader as Mapper
 
 def mainScript(flangeClass, flangeSize, pipeSchedule, gasketThickness):
 
