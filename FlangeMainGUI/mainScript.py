0c139d9d3cbbe10299aa4001901878a96c62b1b8
Josh Collins <jcollins997@yahoo.com>
2020-04-29 17:41:38 -0500

    Added material loading and assignment features.

--- FlangeMainGUI/mainScript.py
+++ FlangeMainGUI/mainScript.py
@@ -3,12 +3,14 @@ import createBolt as Bolt
 import createGasket as Gasket
 import FlangeWithThickComplexHub as Flange
 import csv_reader as Mapper
+import assignMaterials as Materials
 
 
-def mainScript(flangeClass, flangeSize, pipeSchedule, gasketThickness):
+def mainScript(flangeClass, flangeSize, pipeSchedule, gasketThickness, flangeFileName, gasketFileName, boltFileName):
 
 	dimensions = Mapper.mapper(flangeClass, flangeSize)
 	Flange.flange(dimensions.get("flangeDimensions"), pipeSchedule)
 	Bolt.makeBolt(dimensions, gasketThickness)
 	Gasket.makeGasket(dimensions.get("gasketDimensions"), gasketThickness)
 	Assembly.assembly(gasketThickness, dimensions.get("flangeDimensions"))
+	Materials.materials(flangeFileName, gasketFileName, boltFileName)
